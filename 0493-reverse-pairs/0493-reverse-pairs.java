273ffb849adaefeb69643761f74ed5e666544dae
Deep <deepkabariya2308@gmail.com>
2026-07-20 19:45:31 +0530

    Time: 78 ms (17.92%), Space: 58.9 MB (88.85%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -2,69 +2,70 @@ import java.util.*;
 
 class Solution {
 
-    public int reversePairs(int[] nums) {
-        return mergeSort(nums, 0, nums.length - 1);
-    }
+    private void merge(int[] arr, int low, int mid, int high) {
 
-    private int mergeSort(int[] nums, int low, int high) {
-        if (low >= high) {
-            return 0;
-        }
-
-        int mid = low + (high - low) / 2;
-
-        int count = 0;
-
-        count += mergeSort(nums, low, mid);
-        count += mergeSort(nums, mid + 1, high);
-        count += countPairs(nums, low, mid, high);
-        merge(nums, low, mid, high);
-
-        return count;
-    }
-
-    private int countPairs(int[] nums, int low, int mid, int high) {
-        int right = mid + 1;
-        int count = 0;
-
-        for (int i = low; i <= mid; i++) {
-            while (right <= high && (long) nums[i] > 2L * nums[right]) {
-                right++;
-            }
-            count += right - (mid + 1);
-        }
-
-        return count;
-    }
-
-    private void merge(int[] nums, int low, int mid, int high) {
         ArrayList<Integer> temp = new ArrayList<>();
 
         int left = low;
         int right = mid + 1;
 
         while (left <= mid && right <= high) {
-            if (nums[left] <= nums[right]) {
-                temp.add(nums[left]);
-                left++;
+
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left++]);
             } else {
-                temp.add(nums[right]);
-                right++;
+                temp.add(arr[right++]);
             }
         }
 
-        while (left <= mid) {
-            temp.add(nums[left]);
-            left++;
+        while (left <= mid)
+            temp.add(arr[left++]);
+
+        while (right <= high)
+            temp.add(arr[right++]);
+
+        for (int i = low; i <= high; i++)
+            arr[i] = temp.get(i - low);
+    }
+
+    private int countPairs(int[] arr, int low, int mid, int high) {
+
+        int right = mid + 1;
+        int cnt = 0;
+
+        for (int i = low; i <= mid; i++) {
+
+            while (right <= high && (long) arr[i] > 2L * arr[right]) {
+                right++;
+            }
+
+            cnt += right - (mid + 1);
         }
 
-        while (right <= high) {
-            temp.add(nums[right]);
-            right++;
-        }
+        return cnt;
+    }
 
-        for (int i = low; i <= high; i++) {
-            nums[i] = temp.get(i - low);
-        }
+    private int mergeSort(int[] arr, int low, int high) {
+
+        if (low >= high)
+            return 0;
+
+        int mid = low + (high - low) / 2;
+
+        int cnt = 0;
+
+        cnt += mergeSort(arr, low, mid);
+        cnt += mergeSort(arr, mid + 1, high);
+
+        cnt += countPairs(arr, low, mid, high);
+
+        merge(arr, low, mid, high);
+
+        return cnt;
+    }
+
+    public int reversePairs(int[] nums) {
+
+        return mergeSort(nums, 0, nums.length - 1);
     }
 }
